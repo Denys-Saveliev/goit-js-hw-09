2f601e537b580dd3e66e3a5458c7305fd7297b53
Denys-Saveliev <denys.saveliev@ukr.net>
2022-04-02 20:12:45 +0300

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,24 @@
+const startBtnRef = document.querySelector("[data-start]");
+const stopBtnRef = document.querySelector("[data-stop]");
+let timerId = null;
 
+startBtnRef.addEventListener("click", () => {
+   startBtnRef.setAttribute('disabled', '');
+   stopBtnRef.removeAttribute('disabled');
+   
+  timerId = setInterval(() => {    
+   const newColor = getRandomHexColor();
+     document.body.style.backgroundColor = newColor;
+  }, 1000);
+});
+
+
+stopBtnRef.addEventListener("click", () => {
+   stopBtnRef.removeAttribute('disabled');
+   startBtnRef.setAttribute('disabled', '');   
+  clearInterval(timerId);  
+});
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
